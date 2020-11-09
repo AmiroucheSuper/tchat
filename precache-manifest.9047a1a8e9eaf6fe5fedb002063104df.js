031f49aafbb04845af86431752590b6615ee61c4
amirouche-hellal <hellal.amirouche@gmail.com>
2020-11-09 19:31:24 +0100

    make it better

---
+++ precache-manifest.9047a1a8e9eaf6fe5fedb002063104df.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71bad730221d67e987c59c695e0b13b5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "75a0768228bbb56662b9",
+    "url": "/static/css/main.435d5fe5.chunk.css"
+  },
+  {
+    "revision": "32932c6454ec776c4e66",
+    "url": "/static/js/2.fedcd052.chunk.js"
+  },
+  {
+    "revision": "f24b473656bdcdf7a40306c1c14414ef",
+    "url": "/static/js/2.fedcd052.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75a0768228bbb56662b9",
+    "url": "/static/js/main.00ff6c9d.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
